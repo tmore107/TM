ad0bbbce5f0dfb53bbcbf4b506f888156db0ff77
Ajay Sathe <Ajay Sathe@LAPTOP-SCLIVQOO>
2025-03-22 11:16:28 +0530

    Changed By trupti

--- GDemo2/src/com/Add.java
+++ GDemo2/src/com/Add.java
@@ -9,6 +9,10 @@ public class Add {
 	public void m2(){
 		System.out.println("This is m2 method()");
 	}
+	
+	public void m3(){
+		System.out.println("This is m2 method()");
+	}
 	public static void main(String[] args) {
 		System.out.println("This is main class");
                 System.out.println("Main method");
